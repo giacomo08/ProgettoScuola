8cc10ec8eb2cd2052f11f51d9e31f26966ce59e4
FSEVERI\reccanello2998 <FSEVERI\reccanello2998@lmatbi-06.fseveri.it>
2016-02-23 09:06:45 +0100

    initialized repository 2

--- src/progattoscuola/ProgattoScuola.java
+++ src/progattoscuola/ProgattoScuola.java
@@ -15,7 +15,7 @@ public class ProgattoScuola {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello word");
+        System.out.println("Hola word");
         System.out.print("ciao");
     }
     
